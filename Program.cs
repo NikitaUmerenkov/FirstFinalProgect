c3608a9dd11cc889bcddf6c84e6b9d2dfe6400d5
Nikita Umerenkov <Nikita Umerenkov>
2023-01-27 11:06:13 +0300

    Первый итоговый проект

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] Array;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+Array = stroka.Split(' ');
+var result = new string[Array.Length];
+var realSize = 0;
+foreach (var value in Array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
